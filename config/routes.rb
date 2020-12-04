3d4f3d0a6819936a221929ff5d502e613ae5084e
Isabel Frade <fradei1@tcnj.edu>
2020-12-03 22:30:29 -0500

    added more comments, fixed some labels

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ devise_for :users
 devise_scope :user do
   get '/users/sign_out' => 'devise/sessions#destroy'
 end
+#route to all relvant views within the books folder
  resources :books, only: [:index, :show, :search, :new, :create, :edit, :destroy]
 
   root 'books#index'
